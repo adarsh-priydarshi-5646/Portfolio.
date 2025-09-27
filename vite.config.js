fb69213cc2d0909d25118449fa68730248c804dd
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2025-09-27 23:11:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
+let plugins = [react()]
+
+// Only include visualizer in development
+if (process.env.NODE_ENV !== 'production') {
+  const { visualizer } = await import('rollup-plugin-visualizer')
+  plugins.push(visualizer())
+}
+
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ open: true }) // optional: bundle analyzer report open karega
-  ],
-  build: {
-    chunkSizeWarningLimit: 1000, // default 500, ab 1000 kB
-  }
+  plugins
 })
