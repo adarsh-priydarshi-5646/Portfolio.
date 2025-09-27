898415823b16b5f606b63113b8be1cff2a07f8ae
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2025-09-27 22:49:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }) // optional: bundle analyzer report open karega
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // default 500, ab 1000 kB
+  }
 })
